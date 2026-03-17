0eb2d6e5d46756b36ee4bf5b798a0e96b1ac66e3
Harsh Vekriya <harshvekariya203@gmail.com>
2026-03-17 18:17:43 +0530

    update cors settings

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api",
+    baseURL: "https://myraid-internship-assignment.onrender.com/api",
     withCredentials: true
 });
 
